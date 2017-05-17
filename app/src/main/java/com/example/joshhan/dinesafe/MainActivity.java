ab3b04c2aff65ab3108cdafc09b517e02ae1c45f
Joshua Han <hanjo@utschools.ca>
2017-05-16 21:01:11 -0400

    Finally working parser!

--- app/src/main/java/com/example/joshhan/dinesafe/MainActivity.java
+++ app/src/main/java/com/example/joshhan/dinesafe/MainActivity.java
@@ -1,38 +1,70 @@
 package com.example.joshhan.dinesafe;
 
+import android.app.Activity;
+import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    String xmlContent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        File file = new File(getResources().getValue(R.values.test.xml));
-        System.out.println(file.exists());
-        /*Parser parser = new Parser();
-        parser.createFile(file);
-        parser.doc.getDocumentElement().normalize();
-        NodeList nList = parser.doc.getElementsByTagName("ROW");
+        textView = (TextView) findViewById(R.id.xml);
+        try {
+            xmlContent = getEventsFromAnXML(this);
+            textView.setText(xmlContent);
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        Node example = nList.item(0);
-        Element e = (Element) nList;
-        /*Restaurant restaurant = new Restaurant(
-                Integer.parseInt(e.getElementsByTagName("ROW_ID").item(0).getTextContent()),
-                e.getElementsByTagName("ESTABLISHMENT_NAME").item(0).getTextContent(),
-                e.getElementsByTagName("ESTABLISHMENTTYPE").item(0).getTextContent(),
-                e.getElementsByTagName("ESTABLISHMENT_ADDRESS").item(0).getTextContent(),
-                e.getElementsByTagName("ESTABLISHMENT_STATUS").item(0).getTextContent(),
-                Integer.parseInt(e.getElementsByTagName("MINIMUM_INSPECTIONS_PERYEAR").item(0).getTextContent()));
-        System.out.println(restaurant.getName());
-        */
+
+    }
+
+
+    private String getEventsFromAnXML(Activity activity) throws XmlPullParserException, IOException{
+        StringBuffer stringBuffer = new StringBuffer();
+        XmlResourceParser xpp = activity.getResources().getXml(R.xml.test);
+        xpp.next();
+
+        int eventType = xpp.getEventType();
+        while(eventType != XmlPullParser.END_DOCUMENT){
+            if(eventType == XmlPullParser.START_DOCUMENT){
+                stringBuffer.append("----Start XML----");
+            }
+            else if(eventType == XmlPullParser.START_TAG)
+            {
+                stringBuffer.append("\nSTART_TAG: "+xpp.getName());
+            }
+            else if(eventType == XmlPullParser.END_TAG)
+            {
+                stringBuffer.append("\nEND_TAG: "+xpp.getName());
+            }
+            else if(eventType == XmlPullParser.TEXT)
+            {
+                stringBuffer.append("\nTEXT: "+xpp.getText());
+            }
+            eventType = xpp.next();
+        }
+        stringBuffer.append("\n--- End XML ---");
+        return stringBuffer.toString();
     }
 }
+
