81e68d67bc180046d008652053198b69bb5026a2
Joshua Han <hanjo@utschools.ca>
2017-05-18 11:00:21 -0400

    Added Search activity and config

--- app/src/main/java/com/example/joshhan/dinesafe/MainActivity.java
+++ app/src/main/java/com/example/joshhan/dinesafe/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.joshhan.dinesafe;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.TextView;
 
 import org.w3c.dom.Element;
@@ -31,15 +37,24 @@ import javax.xml.parsers.SAXParserFactory;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
-    String xmlContent;
-    SearchView searchBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
         textView = (TextView) findViewById(R.id.xml);
-        try{
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())){
+            String query = intent.getStringExtra(SearchManager.QUERY);
+
+        }
+
+        /*try{
             InputStream is = getResources().openRawResource(R.raw.test);
             Parser parser = new Parser();
             parser.saxParser.parse(is, parser.handler);
@@ -47,8 +62,23 @@ public class MainActivity extends AppCompatActivity {
             System.out.println("Restaurant" + parser.restaurant.toString());
             textView.setText(parser.restaurant.toString());
         }
-        catch(Exception e){e.printStackTrace();}
+        catch(Exception e){e.printStackTrace();}*/
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search_bar).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        return true;
     }
 }
 
