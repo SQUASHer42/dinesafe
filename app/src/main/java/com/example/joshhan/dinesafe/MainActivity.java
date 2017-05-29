3d74a3ca712f1aa2fe377668d9956e0ebee5271b
Alain Lou <loual@utschools.ca>
2017-05-29 11:00:21 -0400

    Location stuff finally works. Need to create a new class for it though.

--- app/src/main/java/com/example/joshhan/dinesafe/MainActivity.java
+++ app/src/main/java/com/example/joshhan/dinesafe/MainActivity.java
@@ -1,21 +1,40 @@
 package com.example.joshhan.dinesafe;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -41,21 +60,15 @@ import java.security.SecureRandom;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, android.location.LocationListener{
 
     TextView textView;
     String xmlContent;
     SearchView searchBar;
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        // Add a marker in Sydney, Australia,
-        // and move the map's camera to the same location.
-        LatLng sydney = new LatLng(-33.852, 151.211);
-        googleMap.addMarker(new MarkerOptions().position(sydney)
-                .title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
+    LocationManager locationManager;
+    Criteria criteria;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,17 +78,24 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        if(googleApiClient == null){
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API).build();
+        }
+
+        googleApiClient.connect();
+
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
         textView = (TextView) findViewById(R.id.xml);
 
         Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())){
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
-
         }
-
         /*try{
             InputStream is = getResources().openRawResource(R.raw.test);
             Parser parser = new Parser();
@@ -86,6 +106,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         catch(Exception e){e.printStackTrace();}*/
 
+    }
+
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -101,6 +141,75 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
         */
         return true;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        try{
+            Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            if (lastLocation != null) {
+                Log.i(TAG, String.valueOf(lastLocation.getLatitude()));
+                TextView view = (TextView) findViewById(R.id.coordinates);
+                view.setText(String.valueOf(lastLocation.getLatitude()) + ", " + String.valueOf(lastLocation.getLongitude()));
+
+                LatLng userLocation = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+                GoogleMap map = mapFragment.getMap();
+                map.addMarker(new MarkerOptions().position(userLocation).title("Where you are"));
+                map.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+            }
+            else {
+                locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+                criteria = new Criteria();
+                String bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true));
+                locationManager.requestLocationUpdates(bestProvider, 1000, 0, this);
+                lastLocation = locationManager.getLastKnownLocation(bestProvider);
+                Log.i(TAG, String.valueOf(lastLocation.getLatitude()));
+                TextView view = (TextView) findViewById(R.id.coordinates);
+                view.setText(String.valueOf(lastLocation.getLatitude()) + ", " + String.valueOf(lastLocation.getLongitude()));
+
+                LatLng userLocation = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+                GoogleMap map = mapFragment.getMap();
+                map.addMarker(new MarkerOptions().position(userLocation).title("Where you are"));
+                map.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+
+            }
+        }
+        catch (SecurityException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
 
