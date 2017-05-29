56b2048404ef57cd6377c107cbc7792f39a9453a
Joshua Han <hanjo@utschools.ca>
2017-05-29 11:25:01 -0400

    Removed old libraries with v7 support libraries

--- app/src/main/java/com/example/joshhan/dinesafe/MainActivity.java
+++ app/src/main/java/com/example/joshhan/dinesafe/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.joshhan.dinesafe;
 
-import android.app.Activity;
+
 import android.app.SearchManager;
+import android.content.ComponentName;
 import android.content.Context;
+
 import android.content.Intent;
-import android.content.res.Resources;
-import android.content.res.XmlResourceParser;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -21,31 +23,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.security.SecureRandom;
 
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     TextView textView;
-    String xmlContent;
-    SearchView searchBar;
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -65,42 +48,30 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-
         textView = (TextView) findViewById(R.id.xml);
-
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())){
-            String query = intent.getStringExtra(SearchManager.QUERY);
-
-        }
-
-        /*try{
-            InputStream is = getResources().openRawResource(R.raw.test);
-            Parser parser = new Parser();
-            parser.saxParser.parse(is, parser.handler);
-
-            System.out.println("Restaurant" + parser.restaurant.toString());
-            textView.setText(parser.restaurant.toString());
-        }
-        catch(Exception e){e.printStackTrace();}*/
-
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the options menu from XML
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+        inflater.inflate(R.menu.options_menu, menu);
 
-        /*// Get the SearchView and set the searchable configuration
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.search_bar).getActionView();
-        // Assumes current activity is the searchable activity
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.search));
+        /*searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(new ComponentName(getApplicationContext(), Search.class)));
         */
-        return true;
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.search){
+            Intent searchIntent = new Intent(this, Search.class);
+            startActivity(searchIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
--- app/src/main/java/com/example/joshhan/dinesafe/Search.java
+++ app/src/main/java/com/example/joshhan/dinesafe/Search.java
@@ -1,13 +1,46 @@
 package com.example.joshhan.dinesafe;
 
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Search extends AppCompatActivity {
+//import java.io.InputStream;
+
+public class Search extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        handleIntent(getIntent());
     }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent){
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())){
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            System.out.println(query);
+        }
+        //getRestaurant();
+    }
+
+    /*private void getRestaurant(){
+        try{
+            InputStream is = getResources().openRawResource(R.raw.test);
+            Parser parser = new Parser();
+            parser.saxParser.parse(is, parser.handler);
+
+            System.out.println("Restaurant" + parser.restaurant.toString());
+            //textView.setText(parser.restaurant.toString());
+        }
+        catch(Exception e){e.printStackTrace();}
+    }*/
 }
